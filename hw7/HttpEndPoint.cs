25062e2504e7ce962a8aaf2695f9001cefc28d3d
jashook <jshook@smu.edu>
2014-09-30 12:04:08 -0500

    Untested adding parameters

--- hw7/HttpEndPoint.cs
+++ hw7/HttpEndPoint.cs
@@ -11,14 +11,16 @@ namespace hw7
 {
    class HttpEndPoint
    {
-      public delegate string OnRequest();
+      public delegate string OnRequest(Dictionary<string, string>);
 
       private OnRequest m_get_request_function;
+      private OnRequest m_post_request_function;
       private string m_url;
 
       public HttpEndPoint(string url, OnRequest get_request, OnRequest post_request)
       {
          m_get_request_function = get_request;
+         m_post_request_function = post_request;
 
          m_url = url;
 
@@ -81,7 +83,25 @@ namespace hw7
          {
             Socket accepting_socket = listening_socket.Accept();
 
-            string html = m_get_request_function();
+            byte[] pre_run_information_buffer = new byte[256];
+
+            int pre_run_amount_read = 0;
+
+            string pre_run_information = "";
+
+            do
+            {
+               pre_run_amount_read = accepting_socket.Receive(pre_run_information_buffer, pre_run_information_buffer.Length, 0);
+
+               pre_run_information = pre_run_information + Encoding.ASCII.GetString(pre_run_information_buffer, 0, pre_run_amount_read);
+
+            } while (pre_run_amount_read > 0);
+
+            Dictionary<string, string> parameters;
+
+            int method = ParseType(pre_run_information, out parameters);
+
+            string html = method == 0 ? m_get_request_function(parameters) : m_post_request_function(parameters);
 
             byte[] html_message = Encoding.ASCII.GetBytes(html);
 
@@ -89,6 +109,28 @@ namespace hw7
 
             accepting_socket.Disconnect(false);
          }
+      }
+
+      private int ParseType(string transfered_information, out Dictionary<string, string> parameters)
+      {
+         parameters = new Dictionary<string, string>();
+
+         char method = transfered_information[0];
+
+         transfered_information = transfered_information.Substring(1);
+
+         if (transfered_information.Length > 0) return (int)method;
+
+         string[] split_information = transfered_information.Split(new char[1] { '&' });
+
+         foreach (string key_value in split_information)
+         {
+            string[] key_values = key_value.Split(new char[1] { '=' });
+
+            parameters.Add(key_values[0], key_values[1]);
+         }
+
+         return (int)method;
       }
    }
 }
